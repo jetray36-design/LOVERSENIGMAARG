8fd4501c45fa58125c98cb27bbaea2a2da61425e
jetray36-design <jetray36@gmail.com>
2025-12-04 18:13:07 +0700

    Create encrypted-dialogue.js

---
+++ encrypted-dialogue.js
@@ -0,0 +1,9 @@
+<script>
+// ===== AES ENCRYPTION SETUP =====
+const encryptedDialogue = `
+U2FsdGVkX1+WciWJXC1X56EDjw7dYj0nT8ZfI5wc0EUe8wskCCJt0LxY3fNCg0d4
+B5afpMIgKM0cT3zOj+71EPFfz/Og6X23zZ/y2wwQFkI0Dz1Q7QBLQ9mNmx8iR3A5
+NGh9NkpdzCq9nqlCxrT84g==
+`;
+// This is just example encrypted text! Replace it with the encrypted output I generate next.
+</script>
