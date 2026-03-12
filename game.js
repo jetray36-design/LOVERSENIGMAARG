e155350c359af43a7dc167f10225e87ce422d7a7
jetray36-design <jetray36@gmail.com>
2026-03-12 15:05:42 +0700

    Update game.js

--- game.js
+++ game.js
@@ -145,7 +145,7 @@ function showRule() {
     print("- This application was made by ${authorName}");
     print("- Experiencing abnormal things while using this application is actually normal");
     print("- You take all responsibilities while using the application");
-    print("- Version 0.3.4");
+    print(updateInstalled ? "- Version 0.3.4-PATCHED" : "- Version 0.3.4");
 }
     
 
@@ -573,6 +573,7 @@ input.addEventListener("keydown", function(e) {
 print("C:\\Lib_LenovoP44\\Users\\663201>Run.exe");
 
 print("Enter passcode.");
+
 
 
 
